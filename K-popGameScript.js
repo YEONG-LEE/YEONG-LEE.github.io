f5bb21173b901a4766155d5bfd738b196010afd2
YEONG LEE <101404969+YEONG-LEE@users.noreply.github.com>
2022-04-12 13:47:52 +0800

    Add files via upload

---
+++ K-popGameScript.js
@@ -0,0 +1,4 @@
+function askName(){
+  var name = prompt("What's your name?");
+  alert("Hello " + name + ", welcome to this puzzle game! Whether you are a k-popper or not, let's test your understanding of K-pop!");
+}
